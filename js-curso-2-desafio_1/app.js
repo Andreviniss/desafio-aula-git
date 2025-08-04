3046bd74f715d52ea0bcaf02c7b911f59a0de949
Andre Vinicius <avss46@gmail.com>
2025-08-04 17:41:14 -0300

    Initial commit

---
+++ js-curso-2-desafio_1/app.js
@@ -0,0 +1,22 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora do Desafio';
+
+function botaoConsole() {
+    alert('O botão foi clicado.');
+}
+
+function botaoAlerta() {
+    alert('Eu amo JS');
+}
+
+function botaoPrompt() {
+    let cidade = prompt('Digite uma cidade no Brasil:');
+    alert(`Estive em ${cidade} e lembrei de você!`);
+}
+
+function botaoSoma() {
+    let n1 = parseInt(prompt('Digite o primeiro número:'));
+    let n2 = parseInt(prompt('Digite o segundo número:'));
+    let resultado = n1 + n2;
+    alert(`A soma dos dois números(${n1}+${n2}) deu ${resultado}.`);
+}
